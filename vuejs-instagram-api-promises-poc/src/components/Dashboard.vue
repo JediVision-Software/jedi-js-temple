41157c1f8cc8fff861a81c95d9c43b60c631a157
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2018-03-09 11:22:25 +0200

    add splitting to 3 groups (euler circles: left, intersaction, right) API based on follows and followed-by API

--- vuejs-instagram-api-promises-poc/src/components/Dashboard.vue
+++ vuejs-instagram-api-promises-poc/src/components/Dashboard.vue
@@ -1,4 +1,5 @@
 <template src="../templates/dashboard.html"></template>
+<style scoped src="../css/dashboard.css"></style>
 <script>
 import Vue from 'vue'
 export default {
@@ -6,28 +7,74 @@ export default {
   data () {
     return {
       endpoints: {
-        followsURL: 'https://api.instagram.com/v1/users/self/follows?access_token='
+        selfURL: 'https://api.instagram.com/v1/users/self?access_token=',
+        followsURL: 'https://api.instagram.com/v1/users/self/follows?access_token=',
+        followedByURL: 'https://api.instagram.com/v1/users/self/followed-by?access_token='
+      },
+      data: {
+        self: {},
+        // 3 groups (euler circles: left, intersaction, right)
+        peopleYouLikeButTheyDontLikeYou: [],
+        peopleMutualLove: [],
+        peopleWhoLikeYouButYouDontLikeThem: []
       }
     }
   },
   methods: {
-    getFollows: function () {
+    splitPeopleTo3Groups: function () {
       var self = this
       var accessToken = Vue.ls.get('accessToken')
-      console.log('DASHBOARD: created, ' + accessToken)
-      self.$http.get(self.endpoints.followsURL + accessToken).then(response => {
-        console.log('---200 OK--')
-        console.log(response)
-        console.log(response.body)
-        console.log('---')
-      }, response => {
-        console.log('---500 not OK --')
-        // error callback
+      // promises
+      var selfPromise = self.$http.get(self.endpoints.selfURL + accessToken).promise
+      var followsPromise = self.$http.get(self.endpoints.followsURL + accessToken).promise
+      var followedByPromise = self.$http.get(self.endpoints.followedByURL + accessToken).promise
+      Promise.all([selfPromise, followsPromise, followedByPromise]).then(values => {
+        // self
+        self.data.self = {
+          username: values[0].body.data.username,
+          full_name: values[0].body.data.full_name
+        }
+        // follows
+        var followsBody = values[1].body.data
+        var followsIds = []
+        var followsMappedById = {}
+        followsBody.forEach(function (item) {
+          var followsId = item.id
+          followsIds.push(followsId)
+          followsMappedById[followsId] = item
+        })
+        // followedBy
+        var followedByBody = values[2].body.data
+        var followedByIds = []
+        var followedByMappedById = {}
+        followedByBody.forEach(function (item) {
+          var followsId = item.id
+          followedByIds.push(followsId)
+          followedByMappedById[followsId] = item
+        })
+        // 3 groups (euler circles: left, intersaction, right)
+        followsIds.forEach(function (followsId) {
+          if (followedByIds.includes(followsId)) {
+            // case: intersaction
+            self.data.peopleMutualLove.push(followsMappedById[followsId])
+            // delete matched userId
+            followedByIds.splice(followedByIds.indexOf(followsId), 1)
+          } else {
+            // case: left
+            self.data.peopleYouLikeButTheyDontLikeYou.push(followsMappedById[followsId])
+          }
+        })
+        followedByIds.forEach(function (followedById) {
+          // case: right
+          self.data.peopleWhoLikeYouButYouDontLikeThem.push(followedByMappedById[followedByIds])
+        })
+      }, reason => {
+        // 404
       })
     }
   },
   created: function () {
-    this.getFollows()
+    this.splitPeopleTo3Groups()
   }
 }
 </script>
--- vuejs-instagram-api-promises-poc/src/components/Home.vue
+++ vuejs-instagram-api-promises-poc/src/components/Home.vue
@@ -9,7 +9,7 @@ export default {
         oauthURL: 'https://api.instagram.com/oauth/authorize?client_id=210a468d3bf14835a73d0d6d01a59cd1&redirect_uri=http://localhost:8080&scope=public_content+follower_list&response_type=token'
       },
       configuration: {
-        timeout: 3000
+        timeout: 500
       }
     }
   },
@@ -20,25 +20,6 @@ export default {
         console.log('Getting client-side (implicit) authentication token')
         window.location.href = self.endpoints.oauthURL
       }, self.configuration.timeout)
-      // self.$http.get(self.endpoints.oauthURL).then(response => {
-      //   console.log('---200 OK--')
-      //   console.log(response)
-      //   console.log(response.body)
-      //   console.log('---')
-      // }, response => {
-      //   console.log('---500 not OK --')
-      //   // error callback
-      // })
-      // self.$http.get(self.endpoints.followedByURL + self.configuration.token).then(response => {
-      //   console.log('---200 OK: Followed By--')
-      //   console.log(response)
-      //   console.log(response.body)
-      //   console.log('---')
-      // }, response => {
-      //   console.log('---500 not OK --')
-      //   console.log(response)
-      //   // error callback
-      // })
     }
   },
   created: function () {
